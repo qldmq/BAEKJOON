9535ce2bfc563eb1550387fd9f9d667d6c428aa1
qldmq <ksh13345@naver.com>
2024-10-20 23:43:09 +0900

    20241020

--- Java/Bronze/1. 입출력과 사칙연산/003.[1001] A-B/Main3.java
+++ Java/Bronze/1. 입출력과 사칙연산/003.[1001] A-B/Main3.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 
-public class N2_1000_AB {
+public class Main3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        System.out.print(a+b);
+        System.out.print(a-b);
 
         sc.close();
     }
