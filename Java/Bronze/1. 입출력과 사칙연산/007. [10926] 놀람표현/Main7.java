7e6d2b04d0c5873fabbdd30501709deaa862d71c
qldmq <ksh13345@naver.com>
2024-10-22 20:54:11 +0900

    20241022

---
+++ Java/Bronze/1. 입출력과 사칙연산/007. [10926] 놀람표현/Main7.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main7 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String name = sc.next();
+
+        sc.close();
+
+        System.out.print(name + "??!");
+    }
+}
