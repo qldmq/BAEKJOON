bfed78b7eb8b8cddd67b8d46d80cddd206fa401d
qldmq <ksh13345@naver.com>
2024-10-22 23:37:37 +0900

    20241022

---
+++ Java/Bronze/1. 입출력과 사칙연산/008.[18108] 연도변경/Main8.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main8 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int year = sc.nextInt();
+
+        sc.close();
+
+        System.out.print(year-543);
+    }
+}
