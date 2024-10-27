0546d85da31dfd45c156feff85b26b8fcb2058d3
qldmq <ksh13345@naver.com>
2024-10-27 23:57:44 +0900

    20241027

---
+++ Java/Bronze/2. 조건문/5. [2884] 알람 시계/Main5.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int h = sc.nextInt();
+        int m = sc.nextInt();
+
+        sc.close();
+
+        m -= 45;
+        if (m < 0) {
+            h -= 1;
+            m = 60+m;
+            if (h<0) {
+                h = 23;
+            }
+        }
+
+        System.out.print(h + " ");
+        System.out.print(m);
+    }
+}
