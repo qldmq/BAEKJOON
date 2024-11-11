f860cb08df52809b3261f72442914d37343dbfd6
qldmq <ksh13345@naver.com>
2024-11-11 21:45:31 +0900

    20241111

---
+++ Java/Bronze/5. 문자열/7. [2675] 문자열 반복/Main7.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main7 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+        
+        for (int i=0; i<t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int r = Integer.parseInt(st.nextToken());
+            String s = st.nextToken();
+            for (int j=0; j<s.length(); j++) {
+                for (int k=0; k<r; k++) {
+                    System.out.print(s.charAt(j));
+                }
+            }
+            System.out.println();
+        }
+    }
+}
