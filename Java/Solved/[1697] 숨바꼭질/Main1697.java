bfd7b9c32b84ac09edb4a26797b65b9abeefe5c5
qldmq <ksh13345@naver.com>
2024-12-05 23:56:45 +0900

    20241205

---
+++ Java/Solved/[1697] 숨바꼭질/Main1697.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main1697 {
+    static int k;
+    static int visited_ary[] = new int[10001];
+
+    public static void BFS(int x) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(x);
+        visited_ary[x] = 1;
+
+        while (!queue.isEmpty()) {
+            int ex = queue.poll();
+            
+            for (int i=0; i<3; i++) {
+                int temp = 0;
+                if (i == 0) {
+                    temp = ex - 1;
+                } else if (i == 1) {
+                    temp = ex + 1;
+                } else {
+                    temp = ex * 2;
+                }
+                if (k == temp) {
+                    System.out.println(visited_ary[ex]);
+                    return;
+                }
+                if (temp >= 0 && temp < visited_ary.length && visited_ary[temp] == 0) {
+                    queue.offer(temp);
+                    visited_ary[temp] = visited_ary[ex] + 1;
+                }
+            }
+        }
+        
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < visited_ary.length; i++) {
+            visited_ary[i] = 0; // 0으로 초기화
+        }
+
+        BFS(n);
+    }
+}
