bf5c3f53870a88a3a2aa1e726aa4d45ef89dcc3d
qldmq <ksh13345@naver.com>
2024-12-13 20:14:12 +0900

    20241213/에러수정정

--- Java/Solved/[7562] 나이트의 이동/Main7562.java
+++ Java/Solved/[7562] 나이트의 이동/Main7562.java
@@ -17,19 +17,26 @@ public class Main7562 {
     public static void BFS (int x, int y) {
         Queue<Point> queue = new LinkedList<>();
         queue.offer(new Point(x, y));
-        visited_ary[x][y] = true;
 
         while(!queue.isEmpty()) {
-            Point now = queue.poll();
-            
+            Point p = queue.poll();
+            int nx = p.x;
+            int ny = p.y;
+
             for (int i=0; i<8; i++) {
-                int xx = x + dx[i];
-                int yy = y + dy[i];
+                int xx = p.x + dx[i];
+                int yy = p.y + dy[i];
+
+                if (xx==rsx && yy==rsy) {
+                    ary[xx][yy] = ary[nx][ny] + 1;
+                    return;
+                }
+
                 if (xx>=0 && yy>=0 && xx<l && yy<l) {
                     if (!visited_ary[xx][yy]) {
-                        visited_ary[x][y] = true;
-                        ary[xx][yy] = ary[xx][yy] + 1;
+                        ary[xx][yy] = ary[nx][ny] + 1;
                         queue.offer(new Point(xx, yy));
+                        visited_ary[xx][yy] = true;
                     }
                 }
             }
@@ -42,20 +49,23 @@ public class Main7562 {
         int t = Integer.parseInt(br.readLine());
 
         for (int i=0; i<t; i++) {
-            for (int j=0; j<3; j++) {
-                l = Integer.parseInt(br.readLine());
+            l = Integer.parseInt(br.readLine());
 
-                ary = new int[l][l];
-                visited_ary = new boolean[l][l];
+            ary = new int[l][l];
+            visited_ary = new boolean[l][l];
 
-                StringTokenizer st = new StringTokenizer(br.readLine());
-                int nowx = Integer.parseInt(st.nextToken());
-                int nowy = Integer.parseInt(st.nextToken());
-                StringTokenizer str = new StringTokenizer(br.readLine());
-                rsx = Integer.parseInt(str.nextToken());
-                rsy = Integer.parseInt(str.nextToken());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int nowx = Integer.parseInt(st.nextToken());
+            int nowy = Integer.parseInt(st.nextToken());
+            StringTokenizer str = new StringTokenizer(br.readLine());
+            rsx = Integer.parseInt(str.nextToken());
+            rsy = Integer.parseInt(str.nextToken());
 
+            if (nowx==rsx && nowy==rsy) {
+                System.out.println(0);
+            } else {
                 BFS(nowx, nowy);
+                System.out.println(ary[rsx][rsy]);
             }
         }
     }
